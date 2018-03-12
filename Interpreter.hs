3d44cd6fad6f2380ca2da2b91a699e81f1f3ba80
mmy97 <mario.micallef.y@gmail.com>
2018-03-12 13:20:20 +0000

    Updated Interpreter

--- Interpreter.hs
+++ Interpreter.hs
@@ -4,9 +4,12 @@ import Grammar
 import Data.List.Split
 import Data.List
 
---helper function
+--helper functions to remove data constructors
 toVarList :: Vars -> [String]
-toVarList (Vars v) = v 
+toVarList (Vars v) = v
+
+toCondList :: Conditions -> [(String, String)]
+toCondList (Equal conds) = [ (v1,v2) | (Var v1, Var v2) <- conds] 
 
 --main ENTRY POINT
 main = do programFile <- openFile "Sample Program.txt" ReadMode
@@ -19,33 +22,34 @@ main = do programFile <- openFile "Sample Program.txt" ReadMode
 
 
 --Interpreter functions (Pattern match AST here)    
-interpret :: Exp -> IO [[String]]
-interpret (Output vars files) = do dat <- storeData files
-                                   let output = orderByOutputVar (matchVars vars dat)
-                                   return output
-
+interpret :: Exp -> IO ([String],[[String]])
+interpret (Output vars (File files)) = do dat <- mapM getDataFromFile files
+                                          let output = permuteOutputRows [(orderByRow (matchVars vars d)) | d <- dat]
+                                          return output
                                    
 --Data Processing functions (e.g lexographical order etc)
 
+ 
+getColByVar :: String -> [(String, [String])] -> [String]
+getColByVar var dat = head [d | (var_file, d) <- dat, var_file == var]
 
-orderByOutputVar :: [[String]] -> [[String]]
-orderByOutputVar dat = [ [ x_i !! index | x_i <- dat] | index <- [0..(length (head dat) -1) ]]
+permuteOutputRows :: [([String], [[String]])] -> ([String],[[String]])
+permuteOutputRows dat = foldr1 foldPermute dat
 
-matchVars :: Vars -> [(String, [String])] -> [[String]]
-matchVars vars dat | checkOutputVarsUsed varList dat
-                     = [d | var_output <- varList, (var_file, d) <- dat, var_output == var_file]
-                   
-                   | otherwise = [] -- ie error because not all output variables have been assigned
+foldPermute :: ([String],[[String]]) -> ([String],[[String]]) -> ([String],[[String]])
+foldPermute (file1Vars, file1Rows) (file2Vars, file2Rows) = ( (file1Vars ++ file2Vars) , [ f1row ++ f2row  | f1row <- file1Rows, f2row <- file2Rows] )
+
+orderByRow :: [(String, [String])] -> ([String],[[String]])
+orderByRow dat = ([ var | (var, x_i) <- dat] , [[ x_i !! index | (var, x_i) <- dat] | index <- [0..(length (snd (head dat)) -1) ]])
+
+matchVars :: Vars -> [(String, [String])] -> [(String,[String])]
+matchVars vars dat = [(var_file, d) | var_output <- varList, (var_file, d) <- dat, var_output == var_file]          
  where varList = toVarList vars 
 
-checkOutputVarsUsed :: [String] -> [(String, [String])] -> Bool
-checkOutputVarsUsed vars dat = and [ or [v == v_file | (v_file,d) <- dat] | v <- vars ]  
+--checkOutputVarsUsed :: [String] -> [(String, [String])] -> Bool
+--checkOutputVarsUsed vars dat = and [ or [v == v_file | (v_file,d) <- dat] | v <- vars ]  
                    
---File Reading Functions                      
-storeData :: Files -> IO [(String, [String])]
-storeData (File files) = getDataFromFile (files !! 0) --temporarily only reading first file due to type errors
-                          
-
+--File Reading Functions                                            
                  
 getDataFromFile :: (Vars, String) -> IO [(String, [String])]
 getDataFromFile (vars, filePath) = getDataFromFile' (toVarList vars) filePath
