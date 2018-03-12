734fb021df6ca7744ca0476978d7756026979b18
mmy97 <mario.micallef.y@gmail.com>
2018-03-12 15:43:33 +0000

    Interpreter working for q1

--- Interpreter.hs
+++ Interpreter.hs
@@ -3,6 +3,7 @@ import Tokens
 import Grammar
 import Data.List.Split
 import Data.List
+import Data.Maybe
 
 --helper functions to remove data constructors
 toVarList :: Vars -> [String]
@@ -22,33 +23,70 @@ main = do programFile <- openFile "Sample Program.txt" ReadMode
 
 
 --Interpreter functions (Pattern match AST here)    
-interpret :: Exp -> IO ([String],[[String]])
-interpret (Output vars (File files)) = do dat <- mapM getDataFromFile files
-                                          let output = permuteOutputRows [(orderByRow (matchVars vars d)) | d <- dat]
-                                          return output
+interpret :: Exp -> IO ([[String]])
+interpret (Output vars (File files)) = do 
+                                          let valid = checkOutputVarsUsed (toVarList vars) files
+                                          if valid then putStr ("") else putStr (show valid)
+                                          dat <- mapM getDataFromFile files --get list of [[var, [column]]] i.e files
+                                          let matched = [ matchVars vars d | d <- dat]
+                                          let ordered = [ orderByRow m | m <- matched, m /= [] ]
+                                          let perm = permuteOutputRows ordered
+                                          let swapped = swap (toVarList vars) (fst perm) (snd perm)
+                                          let sorted = sort swapped
+                                          return sorted
                                    
 --Data Processing functions (e.g lexographical order etc)
 
- 
-getColByVar :: String -> [(String, [String])] -> [String]
-getColByVar var dat = head [d | (var_file, d) <- dat, var_file == var]
-
+--returns all permutations of multiple rows of files joined together
 permuteOutputRows :: [([String], [[String]])] -> ([String],[[String]])
 permuteOutputRows dat = foldr1 foldPermute dat
 
 foldPermute :: ([String],[[String]]) -> ([String],[[String]]) -> ([String],[[String]])
 foldPermute (file1Vars, file1Rows) (file2Vars, file2Rows) = ( (file1Vars ++ file2Vars) , [ f1row ++ f2row  | f1row <- file1Rows, f2row <- file2Rows] )
 
+--combines lists of columns and returns lists of rows
 orderByRow :: [(String, [String])] -> ([String],[[String]])
 orderByRow dat = ([ var | (var, x_i) <- dat] , [[ x_i !! index | (var, x_i) <- dat] | index <- [0..(length (snd (head dat)) -1) ]])
 
+--checks which OUTPUT VARS match the list of [[var, [column]]] and returns the relevant ones
 matchVars :: Vars -> [(String, [String])] -> [(String,[String])]
 matchVars vars dat = [(var_file, d) | var_output <- varList, (var_file, d) <- dat, var_output == var_file]          
  where varList = toVarList vars 
 
---checkOutputVarsUsed :: [String] -> [(String, [String])] -> Bool
---checkOutputVarsUsed vars dat = and [ or [v == v_file | (v_file,d) <- dat] | v <- vars ]  
-                   
+checkOutputVarsUsed :: [String] -> [(Vars, String)] -> Bool
+checkOutputVarsUsed vars dat | valid = True
+                             | otherwise = error "Runtime error: You have tried to output a variable which has not been taken from a file"
+ where valid = and [ or [ elem v (toVarList file_vars) | (file_vars, filePath) <- dat] | v <- vars ]  
+
+--finds which variables have been swapped form the first two list of strings
+--then swaps the respective strings in the given list of lists
+--e.g. swap ["x1", "x2"] ["x2", "x1"] [["a", "b"],["c","d"]]
+-- [["b", "a"], ["d", "c"]]
+swap :: [String] -> [String] -> [[String]] -> [[String]]
+swap originalVars sortedVars rows = swapElementsAtRecursive (findSwaps originalVars sortedVars) rows
+
+--finds the positions at which variables have been swapped
+findSwaps :: [String] -> [String] -> [(Int, Int)]
+findSwaps originalVars sortedVars = removeSwapDuplicates [(one, two) | let varLength = (length originalVars) - 1,
+                                          one <- [0..varLength], two <- [0..varLength],
+                                          originalVars !! one == sortedVars !! two]
+
+--removes swaps are semantically equivalent
+removeSwapDuplicates :: [(Int, Int)] -> [(Int, Int)]
+removeSwapDuplicates pairs = [(first, second) | (first, second) <- pairs, first <= second]
+
+--recursively calls swapElementsAt given multiple positions and multiple lists
+swapElementsAtRecursive :: [(Int, Int)] -> [[String]] -> [[String]]
+swapElementsAtRecursive (x:[]) rows = [swapElementsAt (fst x) (snd x) row | row <- rows]
+swapElementsAtRecursive (x:xs) rows = swapElementsAtRecursive xs [swapElementsAt (fst x) (snd x) row | row <- rows]
+
+--swaps two elements in list given two positions
+swapElementsAt :: Int -> Int -> [a] -> [a]
+swapElementsAt f s xs = map snd . foldr (\x a ->
+        if fst x == f then ys !! s : a
+        else if fst x == s then ys !! f : a
+        else x : a) [] $ ys
+    where ys = zip [0..] xs 
 --File Reading Functions                                            
                  
 getDataFromFile :: (Vars, String) -> IO [(String, [String])]
